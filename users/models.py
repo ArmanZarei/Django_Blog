f960831c202d7607e8d7133760c03394bb9eadb6
ArmanZarei <armanzarei1378@gmail.com>
2022-02-09 10:38:49 +0330

    Add follower and followings relation to Profile

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
+    followings = models.ManyToManyField("Profile", related_name='followers')
 
     def __str__(self):
         return self.user.username + " profile"
